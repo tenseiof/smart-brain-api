e1b661892914318b89140306077b20d08f5c0fd9
tenseiof <edotenseiof@gmail.com>
2025-04-23 20:09:13 +0300

    deploying fix

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const db = knex({
 	connection: {
 		host: process.env.DB_HOST,
 		port: process.env.DB_PORT,
+		connectionString: process.env.DATABASE_URL,
+		ssl: { rejectUnauthorized: false },
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_DATABASE
